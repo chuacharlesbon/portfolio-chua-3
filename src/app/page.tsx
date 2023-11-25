f9ae636b52d88899fa6392cc9a5bd7b305d3d015
Charles Bon Chua <chuacharlesbon@gmail.com>
2023-11-25 20:43:50 +0800

    Updated: Temporary Redirect from Default Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from 'next/image'
+'use client'
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/home'); 
+  }, []);
+
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
