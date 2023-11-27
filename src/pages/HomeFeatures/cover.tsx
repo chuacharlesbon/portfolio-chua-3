7fb87a5387139b4adb77f4c5fae777b08c0a034e
Charles Bon Chua <chuacharlesbon@gmail.com>
2023-11-27 10:46:42 +0800

    Fixes: Cover - temp

--- src/pages/HomeFeatures/cover.tsx
+++ src/pages/HomeFeatures/cover.tsx
@@ -8,10 +8,12 @@ export const CoverFeature: FC<any> = () => {
     return (
         <Div className="w-full py-4 tablet:py-8 laptop:py-12">
             <FlexRow className="w-full items-center">
-                <img
+                <Image
                     src={Images.cover}
                     alt="C Logo"
                     className='object-cover w-full tablet:w-1/2 laptop:w-3/4 mx-auto'
+                    width={50}
+                    height={50}
                 />
             </FlexRow>
             <FlexRow className="w-full">
