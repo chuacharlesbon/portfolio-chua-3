b00fb56a5523a3357a9020d389fa6f903372ba5d
Charles Bon Chua <chuacharlesbon@gmail.com>
2024-01-27 23:01:17 +0800

    Added Custom 404 Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import 'tailwindcss/tailwind.css';
+
+export default function Custom404() {
+  return <div className="min-h-screen w-full bg-dark-100 flex flex-row items-center justify-center">404 - Page Not Found</div>
+}
