c88a515d36337a057faf03be44b565c8564bd76f
Charles Bon Chua <chuacharlesbon@gmail.com>
2024-02-11 20:13:53 +0800

    Updated GTM

--- src/helpers/gtm-script.js
+++ src/helpers/gtm-script.js
@@ -1,10 +1,19 @@
 export const performGTM = () => {
-    console.log("trigger manual gtm");
+    console.log(`trigger manual gtm ${window.location.pathname}`);
     window.dataLayer = window.dataLayer || [];
     function gtag(){window.dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-8EG7WTTNQT', {
     page_path: window.location.pathname,
     });
+    window.dataLayer.push({
+        event: document.title,
+        event_category: 'user_interaction',
+        event_label: 'clicked_button',
+    });
+    window.gtag('config', 'G-8EG7WTTNQT', {
+        page_path: window.location.pathname,
+    });
+    console.log("manual gtm completed");
 }
 
