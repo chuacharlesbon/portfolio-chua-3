25cc9dc66658ede29014b2627e65550d3bb4602f
Charles Bon Chua <chuacharlesbon@gmail.com>
2024-02-12 14:30:20 +0800

    Updated GTM: test About

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,16 +12,44 @@ import { AboutSlider } from '@/components/features/AboutFeatures/slider';
 import { BackButton } from '@/components/features/back_button';
 import { AboutContents } from '@/components/features/AboutFeatures/contents';
 import { performGTM } from '@/helpers/gtm-script';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, isSupported, logEvent } from 'firebase/analytics';
 
-export default function About() {
+export async function getStaticProps() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDyJ77768PKJECg-hHgqGNcnovTSIxiqXs",
+        authDomain: "my-portfolio-73bbd.firebaseapp.com",
+        projectId: "my-portfolio-73bbd",
+        storageBucket: "my-portfolio-73bbd.appspot.com",
+        messagingSenderId: "633292878880",
+        appId: "1:633292878880:web:e0f6c065300d7dd9367845",
+        measurementId: "G-8EG7WTTNQT"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics: any = await isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+    return {
+      // Pass data as a prop to the page component
+      props: {
+        faAnalytics: analytics,
+      },
+    };
+}
+
+export default function About({ faAnalytics, ...otherProps} : {faAnalytics: any;}) {
     const [loading, setIsLoading] = React.useState(false);
     const [initPage, setInitPage] = React.useState(false);
 
     React.useEffect(() => {
         console.log("trigger");
-        if(!initPage){
+        if (!initPage && faAnalytics) {
             setInitPage(true);
-            performGTM();
+            // performGTM();
+            logEvent(faAnalytics, document.title, {
+                path: window.location.pathname,
+            });
         }
     }, [])
 
